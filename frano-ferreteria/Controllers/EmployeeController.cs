4222eede6a3efd6e324a1addb97d1efe622f2a77
andresf0090@gmail.com <andresf0090@gmail.com>
2025-05-04 16:20:30 -0400

    added post route to employees

--- frano-ferreteria/Controllers/EmployeeController.cs
+++ frano-ferreteria/Controllers/EmployeeController.cs
@@ -22,5 +22,24 @@ namespace frano_ferreteria.Controllers
             var employees = await _context.Employees.ToListAsync();
             return Ok(employees);
         }
+
+        [HttpGet("/api/employee/{id}")]
+        public async Task<ActionResult<List<Employee>>> GetEmployeeById(int id) 
+        {
+            var employee = await _context.Employees.FirstOrDefaultAsync(p => p.Id == id);
+            if(employee is null)
+            {
+                return NotFound("Employee not found");
+            }
+            return Ok(employee);
+        }
+        [HttpPost("/api/employee")]
+        public async Task<ActionResult<List<Employee>>> AddEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return Created();
+        }
+
     }
 }
