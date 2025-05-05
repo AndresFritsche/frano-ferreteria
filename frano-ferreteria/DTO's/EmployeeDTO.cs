58166d198967d4b721bcaff769a22248eb5f6991
andresf0090@gmail.com <andresf0090@gmail.com>
2025-05-04 21:06:54 -0400

    added put and delete controller routes

---
+++ frano-ferreteria/DTO's/EmployeeDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace frano_ferreteria.DTO_s
+{
+    public class EmployeeDTO
+    {
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public int Age { get; set; }
+        public DateOnly HiringDate { get; set; }
+
+    }
+}
